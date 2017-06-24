f4c1df0690e94375527ef40dad7d0b0e1d43c83b
eray <akyildiz379@gmail.com>
2017-06-24 21:50:33 +0300

    Request ( Get ) encoded.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var req = require('request');
 
 /* GET home page. */
-router.get('/', (request, response, next) => {
+router.get('/', ( request, response, next ) => {
   response.render('index');
+});
+
+// POST
+router.post('/',( request, response, next ) => {
+
+    var search = request.body.search;
+
+    if( !( search === '' ) )
+    {
+        req(`${search}`, (error, httpResponse, body) => {
+            
+            response.render('index',{
+                error:error,
+                statusCode:httpResponse && httpResponse.statusCode,
+                body:body 
+            });  
+
+        });
+    }
+    else
+        response.redirect('/');
+
 });
 
 module.exports = router;
